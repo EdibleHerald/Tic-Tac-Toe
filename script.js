5bce474eee6909014345735e42f9be27cfa549d9
EdibleHerald <142949302+EdibleHerald@users.noreply.github.com>
2025-03-20 16:22:11 -0400

    Added board setting/resetting functions in dedicated object for cleaner execution

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
-// Create board
 
-function newBoard(){
-    
-    
-    for(let i=0;i<9;i++){
-        let newboard = document.createElement("div");
-        newboard.id = "div"+i;
-        newboard.classList.add("boardDiv");
 
-        let boardDiv = document.getElementById("fullBoardDiv");
-        boardDiv.appendChild(newboard);
+// We need a function to create the board
+
+const board = {
+
+    setBoard: function(){
+        // Create elements
+        for(let i=0;i<9;i++){
+            let newboard = document.createElement("div");
+            newboard.id = "div"+i;
+            newboard.classList.add("boardDiv");
+    
+            let boardDiv = document.getElementById("fullBoardDiv");
+            boardDiv.appendChild(newboard);
+        }
+    },
+    resetBoard: function(){
+        // Wipe attributes
+        for(let i=0;i<9;i++){
+            let selectedTile = document.getElementById("div"+i);
+
+            try{
+                selectedTile.classList.remove("selectedPlayer");
+            }
+            catch(error){
+                console.log("Does not have selectedPlayer class");
+            }
+            
+
+            try{
+                selectedTile.classList.remove("selectedBot");
+            }
+            catch(error){
+                console.log("Does not have selectedBot class");
+            } 
+            finally{
+                continue;
+            }
+        }
     }
+    
 }
 
-newBoard();
+// 
+
+board.setBoard();
+
+const timer = {
+    
+    start: function(){
+
+    }
+}
 
